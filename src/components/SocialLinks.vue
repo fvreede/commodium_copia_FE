efff09925814772d8fdc06bca1c32b2a4f99aabc
Fabio Vreede <8494527+fvreede@users.noreply.github.com>
2024-10-04 08:37:53 +0200

    Added and styled the SocialLinks component with FontAwesome icons for Facebook, Twitter, and Instagram.

---
+++ src/components/SocialLinks.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="space-y-2 bg-slate-200 rounded-lg p-4">
+        <h3 class="text-lg font-semibold text-gray-900">Volg ons:</h3>
+        <div class="space-x-4">
+            <a href="#" class="text-gray-700 hover:text-gray-900">
+                <FontAwesomeIcon :icon="faFacebookSquare" class="w-8 h-8"/>
+            </a>
+            <a href="#" class="text-gray-700 hover:text-gray-900">
+                <FontAwesomeIcon :icon="faXTwitter" class="w-8 h-8"/>
+            </a>
+            <a href="#" class="text-gray-700 hover:text-gray-900">
+                <FontAwesomeIcon :icon="faInstagramSquare" class="w-8 h-8"/>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faFacebookSquare, faXTwitter, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+</script>
